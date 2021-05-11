86b6da9edb7c01217cb0bcb2b3c02e8cc7ad6c3d
Sylvain K <skocher@avb202.com>
2021-05-11 17:06:06 +0200

    Initialize

---
+++ src/main/java/fr/arolla/skocher/diamond/Diamond.java
@@ -0,0 +1,7 @@
+package fr.arolla.skocher.diamond;
+
+import java.util.Arrays;
+
+public class Diamond {
+
+}
---
+++ src/test/java/fr/arolla/skocher/diamond/DiamondTest.java
@@ -0,0 +1,14 @@
+package fr.arolla.skocher.diamond;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DiamondTest {
+
+    @Test
+    public void dummy_test() {
+        assertEquals(true, true);
+    }
+
+}
