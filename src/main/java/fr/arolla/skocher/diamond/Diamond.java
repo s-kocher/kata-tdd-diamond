f208e2c52c06cb22e3de12005f2f63289d65b769
Sylvain K <skocher@avb202.com>
2021-05-13 00:26:43 +0200

    Refacto : generic way to displau the A tip

--- src/main/java/fr/arolla/skocher/diamond/Diamond.java
+++ src/main/java/fr/arolla/skocher/diamond/Diamond.java
@@ -13,30 +13,36 @@ public class Diamond {
     public String display() {
         if (widthLetter == 'D') {
             return "" +
-                getSpaces(3) + "A" + "\n" +
+                displayTip() + "\n" +
                 getSpaces(2) + "B" + getSpaces(1) + "B" + "\n" +
                 getSpaces(1) + "C" + getSpaces(3) + "C" + "\n" +
                 getSpaces(0) + "D" + getSpaces(5) + "D" + "\n" +
                 getSpaces(1) + "C" + getSpaces(3) + "C" + "\n" +
                 getSpaces(2) + "B" + getSpaces(1) + "B" + "\n" +
-                getSpaces(3) + "A";
+                displayTip();
         }
         if (widthLetter == 'C') {
             return "" +
-            getSpaces(2) + "A" + "\n" +
-            getSpaces(1) + "B" + getSpaces(1) + "B" + "\n" +
-            getSpaces(0) + "C" + getSpaces(3) + "C" + "\n" +
-            getSpaces(1) + "B" + getSpaces(1) + "B" + "\n" +
-            getSpaces(2) + "A";
+                displayTip() + "\n" +
+                getSpaces(1) + "B" + getSpaces(1) + "B" + "\n" +
+                getSpaces(0) + "C" + getSpaces(3) + "C" + "\n" +
+                getSpaces(1) + "B" + getSpaces(1) + "B" + "\n" +
+                displayTip();
         }
         if (widthLetter == 'B') {
             return "" +
-                getSpaces(1) + "A" + "\n" +
+                displayTip() + "\n" +
                 getSpaces(0) + "B" + getSpaces(1) + "B" + "\n" +
-                getSpaces(1) + "A";
+                displayTip();
         }
 
-        return "A";
+        return displayTip();
+    }
+
+    private String displayTip() {
+        int depth = Character.getNumericValue(widthLetter) - Character.getNumericValue('A');
+        String leftSpaces = getSpaces(depth);
+        return leftSpaces + "A";
     }
 
     private static String getSpaces(int spacesNumber) {
